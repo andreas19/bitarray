7accaed9cbefce6fa2aee318dfae4024adb8842e
andreas19 <andreas19@posteo.eu>
2023-05-17 17:56:55 +0200

    Prepare release

--- bitarray.go
+++ bitarray.go
@@ -1,1 +1,418 @@
+// Package bitarray implements a [BitArray].
+//
+// The least significant bit is at index 0. In the string representation used by
+// [Parse], [MustParse], and [BitArray.String] it is the rightmost digit.
 package bitarray
+
+import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+	"math"
+	"math/bits"
+	"strings"
+)
+
+const bitsN = 8
+
+// BitArray type.
+type BitArray struct {
+	size int
+	data []uint8
+}
+
+// New creates a new BitArray with size bits and the bits at the given indexes
+// set to 1. Panics if size <= 0 or one of the indexes is out of range.
+func New(size int, idx ...int) *BitArray {
+	if size <= 0 {
+		panic("size must be > 0")
+	}
+	n, r := size/bitsN, size%bitsN
+	if r > 0 {
+		n++
+	}
+	ba := BitArray{size, make([]uint8, n)}
+	for _, i := range idx {
+		ba.Set(i)
+	}
+	return &ba
+}
+
+// Parse creates a new BitArray by parsing the given string. Space characters are ignored.
+// Returns an error if one of the characters in the string is not space, 0, or 1.
+func Parse(s string) (*BitArray, error) {
+	rs := []rune(strings.ReplaceAll(s, " ", ""))
+	rsLen := len(rs)
+	ba := New(len(rs))
+	for i, c := range rs {
+		if c == '1' {
+			ba.set(rsLen - 1 - i)
+		} else if rs[i] != '0' {
+			return nil, fmt.Errorf("unknown character: %c", rs[i])
+		}
+	}
+	return ba, nil
+}
+
+// MustParse creates a new BitArray by parsing the given string. Space characters are
+// ignored. Panics if one of the characters in the string is not space, 0, or 1.
+func MustParse(s string) *BitArray {
+	ba, err := Parse(s)
+	if err != nil {
+		panic(err)
+	}
+	return ba
+}
+
+// Clone clones the BitArray.
+func Clone(ba *BitArray) *BitArray {
+	sl := make([]uint8, len(ba.data))
+	copy(sl, ba.data)
+	return &BitArray{ba.size, sl}
+}
+
+// Clear sets all bits to 0.
+func (ba *BitArray) Clear() {
+	for i := range ba.data {
+		ba.data[i] = 0
+	}
+}
+
+// SetAll sets all bits to 1.
+func (ba *BitArray) SetAll() {
+	if x := ba.size % bitsN; x == 0 {
+		ba.data[len(ba.data)-1] = math.MaxUint8
+	} else {
+		ba.data[len(ba.data)-1] = (2 << (x - 1)) - 1
+	}
+	for i := 0; i < len(ba.data)-1; i++ {
+		ba.data[i] = math.MaxUint8
+	}
+}
+
+// Set sets the bit at index idx to 1.
+func (ba *BitArray) Set(idx int) {
+	ba.checkIdx(idx)
+	ba.set(idx)
+}
+
+func (ba *BitArray) set(idx int) {
+	n, i := idx/bitsN, idx%bitsN
+	ba.data[n] |= 1 << i
+}
+
+// Unset sets the bit at index idx to 0.
+func (ba *BitArray) Unset(idx int) {
+	ba.checkIdx(idx)
+	ba.unset(idx)
+}
+
+func (ba *BitArray) unset(idx int) {
+	n, i := idx/bitsN, idx%bitsN
+	ba.data[n] &^= 1 << i
+}
+
+// Get reports whether the bit at index idx is set.
+func (ba *BitArray) Get(idx int) bool {
+	ba.checkIdx(idx)
+	return ba.get(idx)
+}
+
+func (ba *BitArray) get(idx int) bool {
+	n, i := idx/bitsN, idx%bitsN
+	return ba.data[n]&(1<<i) != 0
+}
+
+// And sets ba = ba & other.
+func (ba *BitArray) And(other *BitArray) {
+	ba.checkSize(other)
+	for i := 0; i < len(ba.data)-1; i++ {
+		ba.data[i] &= other.data[i]
+	}
+	if ba.size%bitsN == 0 {
+		i := len(ba.data) - 1
+		ba.data[i] &= other.data[i]
+	} else {
+		for i := (ba.size / bitsN) * bitsN; i < ba.size; i++ {
+			if !other.get(i) {
+				ba.unset(i)
+			}
+		}
+	}
+}
+
+// Or sets ba = ba | other.
+func (ba *BitArray) Or(other *BitArray) {
+	ba.checkSize(other)
+	for i := 0; i < len(ba.data)-1; i++ {
+		ba.data[i] |= other.data[i]
+	}
+	if ba.size%bitsN == 0 {
+		i := len(ba.data) - 1
+		ba.data[i] |= other.data[i]
+	} else {
+		for i := (ba.size / bitsN) * bitsN; i < ba.size; i++ {
+			if other.get(i) {
+				ba.set(i)
+			}
+		}
+	}
+}
+
+// Xor sets ba = ba ^ other.
+func (ba *BitArray) Xor(other *BitArray) {
+	ba.checkSize(other)
+	for i := 0; i < len(ba.data)-1; i++ {
+		ba.data[i] ^= other.data[i]
+	}
+	if ba.size%bitsN == 0 {
+		i := len(ba.data) - 1
+		ba.data[i] ^= other.data[i]
+	} else {
+		for i := (ba.size / bitsN) * bitsN; i < ba.size; i++ {
+			b1 := ba.get(i)
+			b2 := other.get(i)
+			b := (b1 || b2) && !(b1 && b2)
+			if b && !b1 {
+				ba.set(i)
+			} else if !b && b1 {
+				ba.unset(i)
+			}
+		}
+	}
+}
+
+// Not sets ba = ^ba.
+func (ba *BitArray) Not() {
+	for i := 0; i < len(ba.data)-1; i++ {
+		ba.data[i] = ^ba.data[i]
+	}
+	if ba.size%bitsN == 0 {
+		i := len(ba.data) - 1
+		ba.data[i] = ^ba.data[i]
+	} else {
+		for i := (ba.size / bitsN) * bitsN; i < ba.size; i++ {
+			if ba.get(i) {
+				ba.unset(i)
+			} else {
+				ba.set(i)
+			}
+		}
+	}
+}
+
+// Rotate rotates the bit array by |n| bits. If n > 0 to the left, if n < 0 to the right.
+func (ba *BitArray) Rotate(n int) {
+	n = n % ba.size
+	var aux *BitArray
+	if n > 0 {
+		aux = Slice(ba, ba.size-n, ba.size)
+	} else if n < 0 {
+		aux = Slice(ba, 0, -n)
+	}
+	start, end := ba.moveBits(n)
+	for i := start; i < end; i++ {
+		if aux.get(i - start) {
+			ba.set(i)
+		} else {
+			ba.unset(i)
+		}
+	}
+}
+
+// Shift shifts the bit array by |n| bits. If n > 0 to the left, if n < 0 to the right.
+func (ba *BitArray) Shift(n int) {
+	if n > 0 && n >= ba.size || n < 0 && -n >= ba.size {
+		ba.Clear()
+		return
+	}
+	start, end := ba.moveBits(n)
+	for i := start; i < end; i++ {
+		ba.unset(i)
+	}
+}
+
+func (ba *BitArray) moveBits(n int) (int, int) {
+	if n == 0 {
+		return 0, 0
+	}
+	var start, end int
+	if n > 0 {
+		for i := ba.size - 1; i >= n; i-- {
+			if ba.get(i - n) {
+				ba.set(i)
+			} else {
+				ba.unset(i)
+			}
+		}
+		end = n
+	} else {
+		n = -n
+		for i := 0; i < ba.size-n; i++ {
+			if ba.get(i + n) {
+				ba.set(i)
+			} else {
+				ba.unset(i)
+			}
+		}
+		start = ba.size - n
+		end = ba.size
+	}
+	return start, end
+}
+
+// Equal reports whether the two bit arrays are equal.
+func (ba *BitArray) Equal(other *BitArray) bool {
+	if ba.size != other.size {
+		return false
+	}
+	for i := range ba.data {
+		if ba.data[i] != other.data[i] {
+			return false
+		}
+	}
+	return true
+}
+
+// Count returns the number of set bits.
+func (ba *BitArray) Count() int {
+	cnt := 0
+	for _, x := range ba.data {
+		cnt += bits.OnesCount8(x)
+	}
+	return cnt
+}
+
+// LeadingZeros returns the number of leading unset bits.
+func (ba *BitArray) LeadingZeros() int {
+	cnt := 0
+	for i := len(ba.data) - 1; i >= 0; i-- {
+		if ba.data[i] == 0 {
+			cnt += bitsN
+		} else {
+			cnt += bits.LeadingZeros8(ba.data[i])
+			break
+		}
+	}
+	if x := ba.size % bitsN; x != 0 {
+		cnt -= bitsN - x
+	}
+	return cnt
+}
+
+// TrailingZeros returns the number of trailing unset bits.
+func (ba *BitArray) TrailingZeros() int {
+	cnt := 0
+	for _, x := range ba.data {
+		if x == 0 {
+			cnt += bitsN
+		} else {
+			cnt += bits.TrailingZeros8(x)
+			break
+		}
+	}
+	if cnt > ba.size {
+		cnt = ba.size
+	}
+	return cnt
+}
+
+// Size returns the size of the bit array.
+func (ba *BitArray) Size() int {
+	return ba.size
+}
+
+// String returns a string representation of the bit array.
+func (ba *BitArray) String() string {
+	baLen := len(ba.data)
+	n := ba.size % bitsN
+	if n == 0 {
+		n = bitsN
+	}
+	s := fmt.Sprintf("%0*b", n, ba.data[baLen-1])
+	if baLen > 1 {
+		for i := baLen - 2; i >= 0; i-- {
+			s += fmt.Sprintf("%0*b", bitsN, ba.data[i])
+		}
+	}
+	return s
+}
+
+func (ba *BitArray) checkIdx(idx int) {
+	if idx < 0 || idx >= ba.size {
+		panic("index out of range")
+	}
+}
+
+func (ba *BitArray) checkSize(other *BitArray) {
+	if ba.size != other.size {
+		panic("bit array sizes must be equal")
+	}
+}
+
+// MarshalBinary implements the [encoding/BinaryMarshaler] interface.
+func (ba *BitArray) MarshalBinary() ([]byte, error) {
+	b := new(bytes.Buffer)
+	enc := gob.NewEncoder(b)
+	err := enc.Encode(ba.size)
+	if err != nil {
+		return nil, err
+	}
+	err = enc.Encode(ba.data)
+	if err != nil {
+		return nil, err
+	}
+	return b.Bytes(), nil
+}
+
+// UnmarshalBinary implements the [encoding/BinaryUnmarshaler] interface.
+func (ba *BitArray) UnmarshalBinary(data []byte) error {
+	b := bytes.NewReader(data)
+	dec := gob.NewDecoder(b)
+	err := dec.Decode(&ba.size)
+	if err != nil {
+		return err
+	}
+	err = dec.Decode(&ba.data)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Slice returns a new BitArray with the bits from ba at indexes [start, end).
+func Slice(ba *BitArray, start, end int) *BitArray {
+	ba.checkIdx(start)
+	ba.checkIdx(end - 1)
+	result := New(end - start)
+	idx := 0
+	for i := start; i < end; i++ {
+		if ba.get(i) {
+			result.set(idx)
+		}
+		idx++
+	}
+	return result
+}
+
+// Concat returns a new BitArray with the bits from ba1 and ba2 concatenated.
+func Concat(ba1, ba2 *BitArray) *BitArray {
+	ba := New(ba1.size + ba2.size)
+	n := ba2.size / bitsN
+	for i := 0; i < n; i++ {
+		ba.data[i] = ba2.data[i]
+	}
+	for i := n * bitsN; i < ba2.size; i++ {
+		if ba2.get(i) {
+			ba.set(i)
+		}
+	}
+	i2 := 0
+	for i := ba2.size; i < ba.size; i++ {
+		if ba1.get(i2) {
+			ba.set(i)
+		}
+		i2++
+	}
+	return ba
+}
--- bitarray_test.go
+++ bitarray_test.go
@@ -1,1 +1,558 @@
 package bitarray
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	size := 10
+	tests := []struct {
+		args []int
+		want []uint8
+	}{
+		{[]int{}, []uint8{0, 0}},
+		{[]int{0}, []uint8{1, 0}},
+		{[]int{1}, []uint8{0b10, 0}},
+		{[]int{7, 0}, []uint8{0b10000001, 0}},
+		{[]int{8}, []uint8{0, 1}},
+		{[]int{9}, []uint8{0, 0b10}},
+		{[]int{9, 0}, []uint8{1, 0b10}},
+		{[]int{9, 7, 1}, []uint8{0b10000010, 0b10}},
+		{[]int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}, []uint8{0b11111111, 0b11}},
+	}
+	for i, test := range tests {
+		ba := New(size, test.args...)
+		if got := ba.data; ba.size != size || !reflect.DeepEqual(got, test.want) {
+			t.Errorf("%d: got %d and %v, want %d and %v", i, ba.size, got, size, test.want)
+		}
+	}
+}
+
+func TestNewPanic(t *testing.T) {
+	defer func() { recover() }()
+	New(0)
+	t.Error("did not panic")
+}
+
+func TestMustParse(t *testing.T) {
+	size := 10
+	tests := []struct {
+		arg  string
+		want []uint8
+	}{
+		{"0000000000", []uint8{0, 0}},
+		{"0000000001", []uint8{1, 0}},
+		{"0000000010", []uint8{0b10, 0}},
+		{"0010000001", []uint8{0b10000001, 0}},
+		{"0100000000", []uint8{0, 1}},
+		{"1000000000", []uint8{0, 0b10}},
+		{"1000000001", []uint8{1, 0b10}},
+		{"1010000010", []uint8{0b10000010, 0b10}},
+		{"1111111111", []uint8{0b11111111, 0b11}},
+		{"11 11111111", []uint8{0b11111111, 0b11}},
+	}
+	for i, test := range tests {
+		ba := MustParse(test.arg)
+		if got := ba.data; ba.size != size || !reflect.DeepEqual(got, test.want) {
+			t.Errorf("%d: got %d and %v, want %d and %v", i, ba.size, got, size, test.want)
+		}
+	}
+}
+
+func TestMustParsePanic(t *testing.T) {
+	defer func() { recover() }()
+	MustParse("012")
+	t.Error("did not panic")
+}
+
+func TestString(t *testing.T) {
+	tests := []string{
+		"0000000000",
+		"0000000001",
+		"0000000010",
+		"0010000001",
+		"0100000000",
+		"1000000000",
+		"1000000001",
+		"1010000010",
+		"1111111111",
+		"0101",
+		"01010101",
+		"0101010101010101",
+	}
+	for i, test := range tests {
+		if got := MustParse(test).String(); got != test {
+			t.Errorf("%d: got %q, want %q", i, got, test)
+		}
+	}
+}
+
+func TestClone(t *testing.T) {
+	tests := []string{
+		"0000000000",
+		"0000000001",
+		"0000000010",
+		"0010000001",
+		"0100000000",
+		"1000000000",
+		"1000000001",
+		"1010000010",
+		"1111111111",
+	}
+	for i, test := range tests {
+		ba := MustParse(test)
+		if got := Clone(ba).String(); got != test {
+			t.Errorf("%d: got %q, want %q", i, got, test)
+		}
+	}
+}
+
+func TestClear(t *testing.T) {
+	s := "1010101010"
+	want := "0000000000"
+	ba := MustParse(s)
+	ba.Clear()
+	if got := ba.String(); got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func TestSetAll(t *testing.T) {
+	tests := []struct {
+		size int
+		want string
+	}{
+		{4, "1111"},
+		{8, "11111111"},
+		{10, "1111111111"},
+		{16, "1111111111111111"},
+	}
+	for i, test := range tests {
+		ba := New(test.size)
+		ba.SetAll()
+		if got := ba.String(); got != test.want {
+			t.Errorf("%d: got %q, want %q", i, got, test.want)
+		}
+	}
+}
+
+func TestSet(t *testing.T) {
+	want := "0100000010"
+	ba := New(10, 1)
+	ba.Set(1)
+	ba.Set(8)
+	if got := ba.String(); got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func TestSetIdxNeg(t *testing.T) {
+	defer func() { recover() }()
+	ba := New(4)
+	ba.Set(-1)
+	t.Error("did not panic")
+}
+
+func TestSetIdxSize(t *testing.T) {
+	defer func() { recover() }()
+	ba := New(4)
+	ba.Set(4)
+	t.Error("did not panic")
+}
+
+func TestUnset(t *testing.T) {
+	want := "1000000001"
+	ba := New(10, 9, 0, 7)
+	ba.Unset(7)
+	ba.Unset(8)
+	if got := ba.String(); got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func TestUnsetIdxNeg(t *testing.T) {
+	defer func() { recover() }()
+	ba := New(4)
+	ba.Unset(-1)
+	t.Error("did not panic")
+}
+
+func TestUnsetIdxSize(t *testing.T) {
+	defer func() { recover() }()
+	ba := New(4)
+	ba.Unset(4)
+	t.Error("did not panic")
+}
+
+func TestGet(t *testing.T) {
+	s := "0100110101"
+	tests := []struct {
+		idx  int
+		want bool
+	}{
+		{0, true},
+		{1, false},
+		{2, true},
+		{3, false},
+		{4, true},
+		{5, true},
+		{6, false},
+		{7, false},
+		{8, true},
+		{9, false},
+	}
+	ba := MustParse(s)
+	for i, test := range tests {
+		if got := ba.Get(test.idx); got != test.want {
+			t.Errorf("%d: got %t, want %t", i, got, test.want)
+		}
+	}
+}
+
+func TestGetIdxNeg(t *testing.T) {
+	defer func() { recover() }()
+	ba := New(4)
+	ba.Get(-1)
+	t.Error("did not panic")
+}
+
+func TestGetIdxSize(t *testing.T) {
+	defer func() { recover() }()
+	ba := New(4)
+	ba.Get(4)
+	t.Error("did not panic")
+}
+
+func TestAnd(t *testing.T) {
+	tests := []struct {
+		s1, s2 string
+		want   string
+	}{
+		{"0000", "1111", "0000"},
+		{"1111", "0000", "0000"},
+		{"0101", "0100", "0100"},
+		{"00000000", "11111111", "00000000"},
+		{"11111111", "00000000", "00000000"},
+		{"01010101", "01000110", "01000100"},
+		{"0000000000", "1111111111", "0000000000"},
+		{"1111111111", "0000000000", "0000000000"},
+		{"0101010101", "0100000110", "0100000100"},
+		{"0000000000000000", "1111111111111111", "0000000000000000"},
+		{"1111111111111111", "0000000000000000", "0000000000000000"},
+		{"0101010101010101", "0100000000000110", "0100000000000100"},
+	}
+	for i, test := range tests {
+		ba := MustParse(test.s1)
+		ba.And(MustParse(test.s2))
+		if got := ba.String(); got != test.want {
+			t.Errorf("%d: got %q, want %q", i, got, test.want)
+		}
+	}
+}
+
+func TestAndDiffSize(t *testing.T) {
+	defer func() { recover() }()
+	New(4).And(New(5))
+	t.Error("did not panic")
+}
+
+func TestOr(t *testing.T) {
+	tests := []struct {
+		s1, s2 string
+		want   string
+	}{
+		{"0000", "1111", "1111"},
+		{"1111", "0000", "1111"},
+		{"0101", "0100", "0101"},
+		{"00000000", "11111111", "11111111"},
+		{"11111111", "00000000", "11111111"},
+		{"01010101", "01000110", "01010111"},
+		{"0000000000", "1111111111", "1111111111"},
+		{"1111111111", "0000000000", "1111111111"},
+		{"0101010101", "0100000110", "0101010111"},
+		{"0000000000000000", "1111111111111111", "1111111111111111"},
+		{"1111111111111111", "0000000000000000", "1111111111111111"},
+		{"0101010101010101", "0100000000000110", "0101010101010111"},
+	}
+	for i, test := range tests {
+		ba := MustParse(test.s1)
+		ba.Or(MustParse(test.s2))
+		if got := ba.String(); got != test.want {
+			t.Errorf("%d: got %q, want %q", i, got, test.want)
+		}
+	}
+}
+
+func TestOrDiffSize(t *testing.T) {
+	defer func() { recover() }()
+	New(4).Or(New(5))
+	t.Error("did not panic")
+}
+
+func TestXor(t *testing.T) {
+	tests := []struct {
+		s1, s2 string
+		want   string
+	}{
+		{"0000", "1111", "1111"},
+		{"1111", "0000", "1111"},
+		{"0101", "0100", "0001"},
+		{"00000000", "11111111", "11111111"},
+		{"11111111", "00000000", "11111111"},
+		{"01010101", "01000110", "00010011"},
+		{"0000000000", "1111111111", "1111111111"},
+		{"1111111111", "0000000000", "1111111111"},
+		{"0101010101", "0100000110", "0001010011"},
+		{"0000000000000000", "1111111111111111", "1111111111111111"},
+		{"1111111111111111", "0000000000000000", "1111111111111111"},
+		{"0101010101010101", "0100000000000110", "0001010101010011"},
+	}
+	for i, test := range tests {
+		ba := MustParse(test.s1)
+		ba.Xor(MustParse(test.s2))
+		if got := ba.String(); got != test.want {
+			t.Errorf("%d: got %q, want %q", i, got, test.want)
+		}
+	}
+}
+
+func TestXorDiffSize(t *testing.T) {
+	defer func() { recover() }()
+	New(4).Xor(New(5))
+	t.Error("did not panic")
+}
+
+func TestNot(t *testing.T) {
+	tests := []struct {
+		s    string
+		want string
+	}{
+		{"0000", "1111"},
+		{"1111", "0000"},
+		{"0101", "1010"},
+		{"00000000", "11111111"},
+		{"11111111", "00000000"},
+		{"01010101", "10101010"},
+		{"0000000000", "1111111111"},
+		{"1111111111", "0000000000"},
+		{"0101010101", "1010101010"},
+		{"0000000000000000", "1111111111111111"},
+		{"1111111111111111", "0000000000000000"},
+		{"0101010101010101", "1010101010101010"},
+	}
+	for i, test := range tests {
+		ba := MustParse(test.s)
+		ba.Not()
+		if got := ba.String(); got != test.want {
+			t.Errorf("%d: got %q, want %q", i, got, test.want)
+		}
+	}
+}
+
+func TestRotate(t *testing.T) {
+	tests := []struct {
+		s    string
+		n    int
+		want string
+	}{
+		{"0001", 0, "0001"},
+		{"0001", 1, "0010"},
+		{"0001", 3, "1000"},
+		{"0001", 4, "0001"},
+		{"0001", 5, "0010"},
+		{"0001", -1, "1000"},
+		{"0001", -4, "0001"},
+		{"00000001", 7, "10000000"},
+		{"00000001", 8, "00000001"},
+		{"00000001", 9, "00000010"},
+		{"00000001", -1, "10000000"},
+		{"0100000001", 0, "0100000001"},
+		{"0100000001", 1, "1000000010"},
+		{"0100000001", 2, "0000000101"},
+		{"0100000001", 10, "0100000001"},
+		{"0100000001", 11, "1000000010"},
+		{"0100000001", -1, "1010000000"},
+		{"0100000001", -2, "0101000000"},
+		{"0100000001", -11, "1010000000"},
+		{"0100000001", -12, "0101000000"},
+	}
+	for i, test := range tests {
+		ba := MustParse(test.s)
+		ba.Rotate(test.n)
+		if got := ba.String(); got != test.want {
+			t.Errorf("%d: got %q, want %q", i, got, test.want)
+		}
+	}
+}
+
+func TestShift(t *testing.T) {
+	tests := []struct {
+		s    string
+		n    int
+		want string
+	}{
+		{"0001", 0, "0001"},
+		{"0001", 1, "0010"},
+		{"0001", 3, "1000"},
+		{"0001", 4, "0000"},
+		{"0001", 5, "0000"},
+		{"1000", -1, "0100"},
+		{"1000", -4, "0000"},
+		{"00000001", 7, "10000000"},
+		{"00000001", 8, "00000000"},
+		{"00000001", 9, "00000000"},
+		{"10000000", -1, "01000000"},
+		{"0100000001", 0, "0100000001"},
+		{"0100000001", 1, "1000000010"},
+		{"0100000001", 2, "0000000100"},
+		{"0100000001", 10, "0000000000"},
+		{"0100000001", 11, "0000000000"},
+		{"0100000001", -1, "0010000000"},
+		{"0100000001", -2, "0001000000"},
+		{"0100000001", -11, "0000000000"},
+		{"0100000001", -12, "0000000000"},
+	}
+	for i, test := range tests {
+		ba := MustParse(test.s)
+		ba.Shift(test.n)
+		if got := ba.String(); got != test.want {
+			t.Errorf("%d: got %q, want %q", i, got, test.want)
+		}
+	}
+}
+
+func TestEqual(t *testing.T) {
+	tests := []struct {
+		s1, s2 string
+	}{
+		{"0101", "010"},
+		{"0101", "1010"},
+		{"01010101", "10101010"},
+		{"0101010101", "1010101010"},
+	}
+	for _, test := range tests {
+		ba1a := MustParse(test.s1)
+		ba1b := MustParse(test.s1)
+		ba2a := MustParse(test.s2)
+		ba2b := MustParse(test.s2)
+		if !ba1a.Equal(ba1b) {
+			t.Errorf("%v = %v: got false, want true", ba1a, ba1b)
+		}
+		if !ba2a.Equal(ba2b) {
+			t.Errorf("%v = %v: got false, want true", ba2a, ba2b)
+		}
+		if ba1a.Equal(ba2a) {
+			t.Errorf("%v = %v: got true, want false", ba1a, ba2a)
+		}
+		if ba2a.Equal(ba1a) {
+			t.Errorf("%v = %v: got true, want false", ba2a, ba1a)
+		}
+	}
+}
+
+func TestCount(t *testing.T) {
+	tests := []struct {
+		s    string
+		want int
+	}{
+		{"0000", 0},
+		{"1111", 4},
+		{"0101", 2},
+		{"00000000", 0},
+		{"11111111", 8},
+		{"01010101", 4},
+		{"0000000000", 0},
+		{"1111111111", 10},
+		{"0101010101", 5},
+		{"0000000000000000", 0},
+		{"1111111111111111", 16},
+		{"0101010101010101", 8},
+	}
+	for i, test := range tests {
+		if got := MustParse(test.s).Count(); got != test.want {
+			t.Errorf("%d: got %d, want %d", i, got, test.want)
+		}
+	}
+}
+
+func TestSize(t *testing.T) {
+	want := 4
+	tests := []*BitArray{New(4), MustParse("1010")}
+	for i, test := range tests {
+		if got := test.Size(); got != want {
+			t.Errorf("%d: got %d, want %d", i, got, want)
+		}
+	}
+}
+
+func TestLeadingTrailingZeros(t *testing.T) {
+	tests := []struct {
+		s              string
+		want_l, want_t int
+	}{
+		{"1001", 0, 0},
+		{"0110", 1, 1},
+		{"0000", 4, 4},
+		{"10000001", 0, 0},
+		{"01000010", 1, 1},
+		{"00000000", 8, 8},
+		{"1000000001", 0, 0},
+		{"0100000010", 1, 1},
+		{"0000000000", 10, 10},
+		{"1000000000000001", 0, 0},
+		{"0100000000000010", 1, 1},
+		{"0000000000000000", 16, 16},
+	}
+	for i, test := range tests {
+		ba := MustParse(test.s)
+		got_l := ba.LeadingZeros()
+		got_t := ba.TrailingZeros()
+		if got_l != test.want_l || got_t != test.want_t {
+			t.Errorf("%d: got %d and %d, want %d and %d", i, got_l, test.want_l, got_t, test.want_t)
+		}
+	}
+}
+
+func TestConcat(t *testing.T) {
+	tests := []struct {
+		s1, s2 string
+	}{
+		{"0101", "101"},
+		{"0101", "1010"},
+		{"0101", "10101"},
+		{"01010101", "1010"},
+		{"01010101", "10101010"},
+		{"0101010101", "1010"},
+		{"0101010101", "101010"},
+		{"0101010101", "10101010"},
+	}
+	for i, test := range tests {
+		ba := Concat(MustParse(test.s1), MustParse(test.s2))
+		want := test.s1 + test.s2
+		if got := ba.String(); got != want {
+			t.Errorf("%d: got %q, want %q", i, got, want)
+		}
+	}
+}
+
+func TestMarshalUnmarshal(t *testing.T) {
+	tests := []string{
+		"0101", "01010101", "0101010101", "0101010101010101",
+	}
+	for i, test := range tests {
+		ba1 := MustParse(test)
+		buf, err := ba1.MarshalBinary()
+		if err != nil {
+			t.Fatal(err)
+		}
+		ba2 := new(BitArray)
+		err = ba2.UnmarshalBinary(buf)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if got := ba2.String(); got != test {
+			t.Errorf("%d: got %q, want %q", i, got, test)
+		}
+	}
+}
